aafd0c2a7e34aa6d02af7e86fda33c0cb46d5806
Dobrev,Konstantin K.N. <k.dobrev@student.fontys.nl>
2026-01-07 13:43:23 +0100

    initial create for logic layer unit tests

---
+++ LogicTests/Services/GameServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Services.Tests
+{
+    [TestClass()]
+    public class GameServiceTests
+    {
+        [TestMethod()]
+        public void ValidateGameTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
